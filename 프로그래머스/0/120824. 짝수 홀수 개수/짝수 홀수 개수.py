48ca15763e292fde9de54d648bbc1ba748dce5b2
august <55475872+august-hw2@users.noreply.github.com>
2025-04-08 22:03:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,6 +1,7 @@
 def solution(num_list):
-    even = sum(1 for i in num_list if i%2==0)
-
-    return [even, len(num_list)-even]
+    answer = [0, 0]
+    for i in num_list:
+        answer[i % 2] += 1 #i%2 == 0 혹은 1이기 때문에 배열의 인덱스는 0 또는 1만 나옴
+    return answer
 
 print(solution(num_list=[1, 2, 3, 4, 5]))
