2c070a97a6903a5cba288d5ae8411bac08b1b9b7
august <55475872+august-hw2@users.noreply.github.com>
2025-04-08 15:19:03 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,11 +1,6 @@
 def solution(n, numlist):
-    res = []
 
-    for i in numlist:
-        if i%n == 0:
-            res.append(i)
-
-    return res
+    return [i for i in numlist if i%n == 0]
 
 
 print(solution(n = 3, numlist = [4, 5, 6, 7, 8, 9, 10, 11, 12]))
