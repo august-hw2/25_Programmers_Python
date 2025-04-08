7596499cc1fd8bc7802896d7b56a4a3efe82d72c
august <55475872+august-hw2@users.noreply.github.com>
2025-04-08 22:01:41 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,4 +1,6 @@
 def solution(num_list):
-    return [sum(1 for i in num_list if i%2==0), sum(1 for i in num_list if i%2!=0)]
+    even = sum(1 for i in num_list if i%2==0)
+
+    return [even, len(num_list)-even]
 
 print(solution(num_list=[1, 2, 3, 4, 5]))
