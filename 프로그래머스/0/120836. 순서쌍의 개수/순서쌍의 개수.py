6d6c0e8e9a5b900fee674a3d5dba02e728fc3556
august <55475872+august-hw2@users.noreply.github.com>
2025-04-08 21:28:56 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.11 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,10 +1,12 @@
 def solution(n):
     res = 0
 
-    for i in range(1, n+1):
+    for i in range(1, int(n**0.5)+1): #n의 제곱근까지만 for문 반복.
         if n%i == 0:
-            res += 1
+            res += 2 #순서만 다른 경우가 2가지 있기 때문.
+            if i*i==n:
+                res -= 1 #i가 n의 제곱근이기 때문에, 이건 1가지 경우의 수라서 -1.
 
     return res
-        
+
 print(solution(n=20))
