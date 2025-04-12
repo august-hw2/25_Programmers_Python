a36c6435875d76af8acd97f3f3fd17fbfaccfdc3
august <55475872+august-hw2@users.noreply.github.com>
2025-04-12 23:08:57 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -4,5 +4,5 @@ def solution(rsp):
     #위 경우에 이기게 됨
 
     key = dict(zip(['2','0','5'], ['0','5','2']))
-
+    #return ''.join(key[i] for i in rsp) -> 문자열로 반복가능
     return ''.join(key[i] for i in list(rsp))
