f9fdb23cdc17063e72e25a2fca00c5c12ca61af5
august <55475872+august-hw2@users.noreply.github.com>
2025-04-12 23:19:18 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,2 +1,3 @@
 def solution(a, b):
-    return int(str(a)+str(b)) if int(str(a)+str(b)) > int(str(b)+str(a)) else int(str(b)+str(a))
+    return max(int(str(a)+str(b)), int(str(b)+str(a)))
+    #return int(str(a)+str(b)) if int(str(a)+str(b)) > int(str(b)+str(a)) else int(str(b)+str(a))
