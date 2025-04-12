a597a1793c1e4abd8f77728c16cf4a2ba58c8c4f
august <55475872+august-hw2@users.noreply.github.com>
2025-04-12 23:06:39 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,8 @@
+def solution(rsp):
+    #rsp -> 가위 2, 바위 0, 보 5
+    #값   -> 바위 0, 보 5, 가위 2
+    #위 경우에 이기게 됨
+
+    key = dict(zip(['2','0','5'], ['0','5','2']))
+
+    return ''.join(key[i] for i in list(rsp))
