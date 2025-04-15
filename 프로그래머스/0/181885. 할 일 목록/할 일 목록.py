d6d20283f6990e79867905ae44627a1df84b7bde
august <55475872+august-hw2@users.noreply.github.com>
2025-04-15 21:22:26 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,9 +1,3 @@
 def solution(todo_list, finished):
 
-    res = []
-
-    for i in range(len(finished)):
-        if finished[i] == False:
-            res.append(todo_list[i])
-
-    return res
+    return [i for i, j in zip(todo_list, finished) if not j]
