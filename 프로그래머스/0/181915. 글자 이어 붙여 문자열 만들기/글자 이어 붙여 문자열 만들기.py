406ed959867cdce4f6274dbe425e89b615f46b0d
august <55475872+august-hw2@users.noreply.github.com>
2025-04-17 22:08:21 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.05 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,7 +1,2 @@
 def solution(my_string, index_list):
-    res = []
-
-    for i in index_list:
-        res.append(my_string[i])
-
-    return ''.join(res)
+    return ''.join([my_string[i] for i in index_list])
