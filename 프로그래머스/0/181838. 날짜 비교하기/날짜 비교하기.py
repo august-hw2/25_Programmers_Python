db9a0179f5e7bb793a471e60fb35de4df9260935
august <55475872+august-hw2@users.noreply.github.com>
2025-04-19 11:02:26 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,7 +1,2 @@
 def solution(date1, date2):
-    for i, j in zip(date1, date2):
-        if i < j:
-            return 1
-        elif i > j:
-            return 0
-    return 0
+    return int(date1 < date2)
