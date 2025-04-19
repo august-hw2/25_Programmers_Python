861026c799efe0e602e735537fa60e47059d3e25
august <55475872+august-hw2@users.noreply.github.com>
2025-04-19 10:42:11 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.20 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -1,2 +1,9 @@
 def solution(board, k):
-    return sum([board[i][j] for i in range(len(board)) for j in range(len(board[0]))if i+j<=k])
+    res = 0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i+j <= k:
+                res += board[i][j]
+            else:
+                break
+    return res
