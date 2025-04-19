db30087fec76e9d779ef10eaafbd99885fb13213
august <55475872+august-hw2@users.noreply.github.com>
2025-04-19 16:26:52 +0900

    [level 0] Title: 배열 만들기 5, Time: 6.45 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,6 +1,2 @@
 def solution(intStrs, k, s, l):
-    res = []
-    for i in intStrs:
-        if int(i[s:s+l]) > k:
-            res.append(int(i[s:s+l]))
-    return res
+    return [int(i[s:s+l]) for i in intStrs if int(i[s:s+l]) > k]
