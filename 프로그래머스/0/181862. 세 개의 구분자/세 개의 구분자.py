8328c432bcdeaae0dfef7f152d19a5df6636f45e
august <55475872+august-hw2@users.noreply.github.com>
2025-04-22 21:14:31 +0900

    [level 0] Title: 세 개의 구분자, Time: 25.48 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,5 +1,3 @@
 import re
 def solution(myStr):
-    tmp = re.split(r'[abc]', myStr)
-    res = list(filter(None, tmp))
-    return res if res else ["EMPTY"]
+    return [x for x in re.split(r'[abc]', myStr) if x] or ["EMPTY"]
