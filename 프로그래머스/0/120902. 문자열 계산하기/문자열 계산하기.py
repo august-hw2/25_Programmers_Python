e0e51cebf1524c26597ff984823e8c882e66fbf2
august <55475872+august-hw2@users.noreply.github.com>
2025-04-24 22:04:27 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,11 @@
+import operator
+
+def solution(my_string):
+    return eval(my_string)
+    
+    #ops = {
+    #    '+': operator.add,
+    #    '-': operator.sub
+    #}
+    #a, op, b = my_string.split()
+    #return ops[op](int(a), int(b))
