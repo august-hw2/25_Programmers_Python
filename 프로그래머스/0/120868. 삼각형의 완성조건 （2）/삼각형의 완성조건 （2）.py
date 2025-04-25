04896508b6dbdef9370141c75d6bdb17be3a94a0
august <55475872+august-hw2@users.noreply.github.com>
2025-04-25 22:32:03 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.08 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,21 +1,14 @@
 def solution(sides):
-    return 2 * min(sides) -1
+    res = 0
+    m = max(sides)
+    n = min(sides)
 
-print(solution(sides=[1, 2]))
-"""
-i) c > b 인 경우 (새로운 변이 가장 긴 경우)
-c > b > a , c < a + b => a < b < c < a + b 이므로
-이 경우 c가 될 수 있는 정수의 개수는 (a + b) - b - 1 = a - 1 로
-a - 1 개이다.
+    # sides에 있는 변(max_sides)이 가장 길 경우
+    for new_side in range(m - n + 1, m + 1):
+        res += 1
 
-ii) c < b 인 경우 (기존의 두 변 중 하나가 가장 긴 경우)
-c < b, a < b, b < a + c => c < b < a + c 이므로
-이 경우 c가 될 수 있는 정수의 개수는 (a + c) - c - 1 = a - 1 로
-a - 1 개이다.
+    # 새로운 변(new_side)가 가장 길 경우
+    for new_side in range(m + 1, m + n):
+        res += 1
 
-iii) c = b 인 경우
-이 경우 c가 될 수 있는 정수의 개수는 1개이다.
-
-따라서, c가 될 수 있는 정수의 총 개수는
-(a - 1) + (a - 1) + 1 = 2a - 1 개이다.
-"""
+    return res
