6c576d8914b1135a49074214ce6cf99f33f09fcb
august <55475872+august-hw2@users.noreply.github.com>
2025-05-15 23:24:26 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.95 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,10 +1,12 @@
 def solution(arr, k):
     tmp = []
-    
+    seen = set()
+
     for i in arr:
-        if i not in tmp:
+        if i not in seen:
+            seen.add(i)
             tmp.append(i)
         if len(tmp) == k:
             break
-    
+
     return tmp + [-1] * (k-len(tmp))
