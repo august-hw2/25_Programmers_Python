e8ed342e17785c12ea014c819948317aef08f582
august <55475872+august-hw2@users.noreply.github.com>
2025-05-18 13:54:22 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.08 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,7 +1,14 @@
+from collections import Counter
+
 def solution(array):
 
-    while len(array) != 0:
-        for i, a in enumerate(set(array)):
-            array.remove(a)
-        if i == 0 : return a
-    return -1
+    if not array:
+        return -1
+
+    cnt = Counter(array)
+    most_cnt = cnt.most_common(2) #가장 많이 나온 값 2개
+
+    if len(most_cnt) == 1 or most_cnt[0][1] > most_cnt[1][1]:
+        return most_cnt[0][0]
+    else:
+        return -1
