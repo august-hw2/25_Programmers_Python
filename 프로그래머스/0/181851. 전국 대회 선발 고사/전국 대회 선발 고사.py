7c93b44b38657faa0572661d2e2ea47993ff3014
august <55475872+august-hw2@users.noreply.github.com>
2025-05-19 21:51:03 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,10 +1,6 @@
 def solution(rank, attendance):
-    tmp = []
 
-    for r, a in zip(rank, attendance):
-        if a:
-            tmp.append(r)
+    answer = sorted((r, i) for i, (r, a) in enumerate(zip(rank, attendance)) if a)
+    a, b, c = answer[0][1], answer[1][1], answer[2][1]
 
-    tmp.sort()
-
-    return 10000*rank.index(tmp[0]) + 100*rank.index(tmp[1]) + rank.index(tmp[2])
+    return 10000*a + 100*b + c
