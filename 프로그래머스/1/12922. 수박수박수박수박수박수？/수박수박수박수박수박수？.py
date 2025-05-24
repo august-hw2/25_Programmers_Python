10f4967986ee9bb54cfbd2c2591e95f799c45392
august <55475872+august-hw2@users.noreply.github.com>
2025-05-24 19:11:51 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.25 ms, Memory: 9.86 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,7 +1,2 @@
 def solution(n):
-    answer = []
-
-    for i in range(n):
-        answer.append('박') if i%2 else answer.append('수')
-
-    return ''.join(answer)
+    return ''.join(['수박'[i % 2] for i in range(n)])
