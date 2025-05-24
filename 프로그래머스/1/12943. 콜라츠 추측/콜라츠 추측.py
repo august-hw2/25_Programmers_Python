2b8dcd00cb476d0f8edabadd5b6d707c785441e2
august <55475872+august-hw2@users.noreply.github.com>
2025-05-24 18:51:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,20 +1,9 @@
 def solution(num):
-    cnt = 0
-    
-    if num == 1:
-        return cnt
-    
-    while True:
-
-        if num%2 == 0: #짝수
-            num //= 2
-        else: #홀수
-            num = num*3 + 1
-    
-        cnt += 1
-
+    for cnt in range(501):
+        
         if num == 1:
             return cnt
-
-        if cnt == 500:
-            return -1
+        
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        
+    return -1
