8942c637e98847b41022a2bb95d71bc571b2c29a
august <55475872+august-hw2@users.noreply.github.com>
2025-05-25 20:20:02 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.45 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,23 +1,11 @@
-def count(num):
-
-    cnt = []
-
-    for i in range(1, int(num**(1/2) + 1)):
-        if num%i==0:
-            cnt.append(i)
-            if i**2 != num:
-                cnt.append(num//i)
-
-    return len(cnt)
-
 def solution(left, right):
     answer = 0
 
     for i in range(left, right+1):
-        j = count(i)
-        if j%2: #홀수
-            answer -= i
-        else: #짝수
-            answer += i
+        # 제곱수인 경우가 해당되면, 무조건 약수 개수가 홀수임
+        if int(i ** 0.5) == i ** 0.5:
+            answer -= i  # 제곱수면 빼기
+        else:
+            answer += i  # 아니면 더하기
 
     return answer
