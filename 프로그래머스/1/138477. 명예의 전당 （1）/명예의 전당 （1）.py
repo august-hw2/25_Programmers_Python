b3f5f176f808a20779187907707a5d56d0c28310
august <55475872+august-hw2@users.noreply.github.com>
2025-05-29 20:26:23 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.22 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,10 @@
+def solution(k, score):
+    answer = []
+    tmp = []
+    for i in score:
+        tmp.append(i)
+        if len(tmp) > k:
+            tmp.remove(min(tmp))
+
+        answer.append(min(tmp))
+    return answer
