f03db494b9ea1a65e3aa8899428f88551b67b9bd
august <55475872+august-hw2@users.noreply.github.com>
2025-05-31 20:32:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 1872.21 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,19 @@
+def count(num):
+
+    cnt = 0
+
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
+            cnt += 1
+            if i ** 2 != num:
+                cnt += 1
+
+    return cnt
+
+def solution(number, limit, power):
+    answer = []
+
+    for i in range(1, number+1):
+        answer.append(count(i) if count(i) < limit+1 else power)
+
+    return sum(answer)
