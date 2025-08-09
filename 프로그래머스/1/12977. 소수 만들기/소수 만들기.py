c23ddb6a23950b150d55ededbbe14340e50fa378
august <55475872+august-hw2@users.noreply.github.com>
2025-08-09 20:11:53 +0900

    [level 1] Title: 소수 만들기, Time: 15.56 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -2,16 +2,21 @@ import math
 from itertools import combinations
 
 def solution(nums):
-    check = list(combinations(nums, 3))
 
-    answer = len(check)
+    answer = 0
 
-    for i in check:
-        tmp = sum(i)
+    for comb in combinations(nums, 3):
+
+        tmp = sum(comb)
+
+        is_prime = True
+
         for j in range(2, int(math.sqrt(tmp)) + 1):
             # x가 해당 수로 나누어떨어진다면
             if tmp % j == 0: # 소수가 아니므로, 전체 숫자 수에서 1개씩 빼기
-                answer -= 1
+                is_prime = False
                 break
+        if is_prime:
+            answer += 1
 
     return answer
