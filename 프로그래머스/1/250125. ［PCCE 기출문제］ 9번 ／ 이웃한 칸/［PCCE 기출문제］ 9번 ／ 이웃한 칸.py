1815feec4727c0f3e8f97ab99acbd2d5460caed5
august <55475872+august-hw2@users.noreply.github.com>
2025-08-15 10:12:26 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -5,22 +5,10 @@ def solution(board, h, w):
     color = board[h][w]
     H, W = len(board), len(board[0])
 
-    # 상
-    if h - 1 >= 0 and board[h - 1][w] == color:
-        answer += 1
-
-    # 하
-    if h + 1 < H and board[h + 1][w] == color:
-        answer += 1
-
-    # 좌
-    if w - 1 >= 0 and board[h][w - 1] == color:
-        answer += 1
-
-    # 우
-    if w + 1 < W and board[h][w + 1] == color:
-        answer += 1
-
-    return answer
+    #상 하 좌 우 순서로 확인 + 리스트 크기 범위인지 확인
+    for idxh, idxw in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nh, nw = h + idxh, w + idxw
+        if 0 <= nh < H and 0 <= nw < W and board[nh][nw] == color:
+            answer += 1
 
     return answer
