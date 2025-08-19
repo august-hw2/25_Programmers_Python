5909e8bd595ba1749322b8e536245e8fc26ab690
august <55475872+august-hw2@users.noreply.github.com>
2025-08-19 20:43:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.16 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,34 +1,16 @@
-
 def solution(survey, choices):
-    answer = []
-
-    case = { # 유형별 점수표
-        'R': 0, 'T': 0,
-        'C': 0, 'F': 0,
-        'J': 0, 'M': 0,
-        'A': 0, 'N': 0,
-    }
-
-    point = { # 매우 비동의 ~ 매우 동의
-        1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3
-    }
+    score = {k: 0 for k in "RTCFJMAN"}  # 점수판
 
     for s, c in zip(survey, choices):
-        one, two = s[0], s[1]
-        if c in (1, 2, 3):
-            case[one] += point[c]
-        elif c in (5, 6, 7):
-            case[two] += point[c]
+        a, b = s[0], s[1]
+        d = c - 4
+        if d < 0:
+            score[a] += -d
+        elif d > 0:
+            score[b] += d
+        # d == 0이면 아무도 가산점 없음
 
-    # 각 지표를 쌍으로 구성한 뒤, 큰 값인 문자 더하기
     pairs = [('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N')]
 
-    for a, b in pairs:
-        if case[a] > case[b]:
-            answer.append(a)
-        elif case[a] < case[b]:
-            answer.append(b)
-        else:
-            answer.append(min(a, b))
-
-    return ''.join(answer)
+    # 동점이면 쌍에서 앞의 문자를 선택(문제의 사전순 규칙과 동일)
+    return ''.join(a if score[a] >= score[b] else b for a, b in pairs)
