0b4a3453652dfc9ef220e81d53acb9d30782d212
august <55475872+august-hw2@users.noreply.github.com>
2025-09-04 20:17:31 +0900

    [level 1] Title: 문자열 나누기, Time: 1.53 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,23 +1,19 @@
 def solution(s):
     answer = 0
-    # 기준 문자 개수, 다른 문자 개수
-    x = y = 0
+    # 기준 문자 (+1), 다른 문자 (-1)
+    bal = 0
+    base = None # 현재 구간의 기준 문자
 
     for ch in s:
-        if x == 0:
-            tmp = ch
-            x += 1
-        else:
-            if tmp == ch:
-                x += 1
-            else:
-                y += 1
-
-        if x == y:
+        if bal == 0:
+            base = ch
+        bal += 1 if ch == base else -1
+        
+        if bal == 0:
             answer += 1
-            x = y = 0
             
-    if x != y:
+    # 끝났는데, 균형 안 맞으면 마지막 구간 1개 추가        
+    if bal != 0:
         answer += 1
 
     return answer
